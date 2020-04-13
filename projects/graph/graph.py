6296956aa4a739af4069a3d229f12588d60ea250
alxanderpierre <alxanderpierre@users.noreply.github.com>
2020-04-13 17:19:06 -0400

    vertices test ok

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -7,25 +7,32 @@ class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.vertices = {}
+        self.vertices = {} # this is our adjacency list
 
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
-        Add a directed edge to the graph.
+        Add a directed edge to the graph from v1 to v2.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            # add edge (line that connects the nodes)
+            self.vertices[v1].add(v2)
+        else:
+            print("ERROR ADDING EDGE: Vertex not found")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        if self.vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        else:
+            return None
 
     def bft(self, starting_vertex):
         """
