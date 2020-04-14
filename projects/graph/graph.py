676cbef83d833ebfc5b430dbd22a032514beb68f
alxanderpierre <alxanderpierre@users.noreply.github.com>
2020-04-14 00:01:32 -0400

    btt

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -10,7 +10,7 @@ class Graph:
         self.vertices = {} # this is our adjacency list
 
     def add_vertex(self, vertex_id):
-        """
+         """
         Add a vertex to the graph.
         """
         self.vertices[vertex_id] = set()
@@ -39,7 +39,28 @@ class Graph:
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+
+        # Create a q and enqueue starting vertex
+        qq = Queue()
+        qq.enqueue([starting_vertex])
+        # Create a set of traversed vertices
+        visited = set()
+        # While queue is not empty:
+        while qq.size() > 0:
+            # dequeue/pop the first vertex
+            path = qq.dequeue()
+            # if not visited
+            if path[-1] not in visited:
+                # DO THE THING!!!!!!!
+                print(path[-1])
+                # mark as visited
+                visited.add(path[-1])
+                # enqueue all neightbors
+                for next_vert in self.get_neighbors(path[-1]):
+                    new_path = list(path)
+                    new_path.append(next_vert)
+                    qq.enqueue(new_path)
+
 
     def dft(self, starting_vertex):
         """
